f51a7aed4b1222ba668b067a0ec5d5f7773dbdd5
Dushyant Pratap Singh <singhdushyant2901@gmail.com>
2022-10-06 12:36:44 +0530

    question3 solved

--- interview-practice-questions/index.js
+++ interview-practice-questions/index.js
@@ -1,26 +1,18 @@
 const input = document.querySelector("#input");
-const incr = document.querySelector("#incr");
-const decr = document.querySelector("#decr");
+const h1 = document.querySelector("#h1");
+const h2 = document.querySelector("#h2");
+const h3 = document.querySelector("#h3");
 const output = document.querySelector("#output");
 
-let fSize = 16;
-function showRes(val) {
-  output.innerText = input.value;
-  output.style.fontSize = `${val}px`;
+function alterText(type) {
+  output.innerHTML = `<${type}>${input.value}<${type}/>`;
 }
-function alterSize(type) {
-  if (type === "incr") {
-    fSize += 2;
-  }
-  if (type === "decr") {
-    fSize -= 2;
-  }
-
-  showRes(fSize);
-}
-incr.addEventListener("click", () => {
-  alterSize("incr");
+h1.addEventListener("click", () => {
+  alterText("h1");
 });
-decr.addEventListener("click", () => {
-  alterSize("decr");
+h2.addEventListener("click", () => {
+  alterText("h2");
+});
+h3.addEventListener("click", () => {
+  alterText("h3");
 });
